b7abe177ededce1a2087fdb7654df4f4ee649ce2
DimMalysh <d.malysh@mail.ru>
2018-03-20 13:28:35 +0200

    Single responsibility principle

---
+++ SingleResponsibilityPrinciple.playground/Contents.swift
@@ -0,0 +1,56 @@
+// Single responsibility principle - Принцип единственной обязанности
+
+protocol CanBeOn {
+    func on()
+}
+
+protocol CanBeOff {
+    func off()
+}
+
+class Switch: CanBeOff, CanBeOn {
+    var stateOn = false
+    
+    func on() {
+        stateOn = true
+    }
+    
+    func off() {
+        stateOn = false
+    }
+}
+
+class SwitchOn {
+    let switcher: CanBeOn
+    
+    init(switcher: CanBeOn) {
+        self.switcher = switcher
+    }
+    
+    func execute() {
+        switcher.on()
+    }
+}
+
+class SwitchOff {
+    let switcher: CanBeOff
+    
+    init(switcher: CanBeOff) {
+        self.switcher = switcher
+    }
+    
+    func execute() {
+        switcher.off()
+    }
+}
+
+let switcher = Switch()
+switcher.stateOn
+
+let switcherOn = SwitchOn(switcher: switcher)
+switcherOn.execute()
+switcher.stateOn
+
+let switcherOff = SwitchOff(switcher: switcher)
+switcherOff.execute()
+switcher.stateOn
